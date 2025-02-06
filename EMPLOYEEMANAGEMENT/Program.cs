3fa90a1f34d2ab48922f052ea925872d15394914
Sri Rajendra Turaga <Rajendratm120@gmail.com>
2025-02-06 14:31:52 +0530

    changes

--- EMPLOYEEMANAGEMENT/Program.cs
+++ EMPLOYEEMANAGEMENT/Program.cs
@@ -1,29 +1,3 @@
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
 using EMPLOYEEMANAGEMENT;
 using EMPLOYEEMANAGEMENT.Models;
 
@@ -31,7 +5,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-       CreateHostBuilder(args).Build().Run();
+        CreateHostBuilder(args).Build().Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
